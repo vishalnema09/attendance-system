1ec3efb71f1adcae66e1a72d01f4bd81defcca00
vishal nema <vishalnema23@gmail.com>
2025-04-19 17:33:19 +0530

    Update adminController.js

--- attendance-system-backend/src/controllers/adminController.js
+++ attendance-system-backend/src/controllers/adminController.js
@@ -73,9 +73,6 @@ exports.loginAdmin = async (req, res) => {
     );
 
     res.cookie("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
